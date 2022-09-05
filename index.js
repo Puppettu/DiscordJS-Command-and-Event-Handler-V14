844c21fdf857e14a43ef4248b572fcab89edadf3
Puppettu <97049995+Puppettu@users.noreply.github.com>
2022-09-05 16:28:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ client.config = require("./config.json");
 client.events = new Collection();
 client.commands = new Collection();
 
-// MongoDB (Watch Lyxcode's Mongo Database Connection | Discord.JS V14 Series | #4)
+// MongoDB (https://www.youtube.com/watch?v=BEkyfqlbVRw)
 //const { connect } = require("mongoose");
 //connect(client.config.DatabaseURL, {
 //}).then(() => console.log("The client is now connected to the database!"));
